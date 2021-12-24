3fee7f8bc828f742f65e9a39bb06176acd8ea627
XenithL <90138611+XenithL@users.noreply.github.com>
2021-12-24 14:34:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
+import flask
 import requests
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, redirect
 import psycopg2
-from werkzeug.utils import redirect
+
+alphabet = 'qwertyuiopasdfghjklzxcvbnm1234567890()!@#$%*{}[]|\?/`_-+=.'
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 conn = psycopg2.connect(database="service_db",
-                        user="<XenithL>",
-                        password="0000",
+                        user="postgres",
+                        password="1234",
                         host="localhost",
                         port="5432")
-
 cursor = conn.cursor()
+
 
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
@@ -22,14 +20,20 @@ def login():
         if request.form.get("login"):
             username = request.form.get('username')
             password = request.form.get('password')
-            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-            records = list(cursor.fetchall())
+            a = [check(username, 0), check(password, 0)]
 
-            return render_template('account.html', full_name=records[0][1])
+            if a.count('good') != 2:
+                return render_template('login.html', attention=a[0] if a[0] != 'good' else a[1])
+            else:
+                cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s",
+                               (str(username), str(password)))
+                records = list(cursor.fetchall())
+                return render_template('account.html', full_name=records[0][1])
         elif request.form.get("registration"):
             return redirect("/registration/")
 
     return render_template('login.html')
+
 
 @app.route('/registration/', methods=['POST', 'GET'])
 def registration():
@@ -38,10 +42,40 @@ def registration():
         login = request.form.get('login')
         password = request.form.get('password')
 
-        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
-                       (str(name), str(login), str(password)))
-        conn.commit()
+        list_ = [name, login, password]
+        for i, line in enumerate(list_):
+            list_[i] = check(line, i)
+            if list_[i] != 'good':
+                return render_template('registration.html', attention=list_[i])
+        if list_.count('good') == 3:
+            cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                           (str(name), str(login), str(password)))
+            conn.commit()
 
-        return redirect('/login/')
+            return redirect('/login/')
 
     return render_template('registration.html')
+
+
+def check(line, num):
+    if num == 1:
+        cursor.execute("SELECT login FROM service.users WHERE login=%s",
+                       (str(line),))
+        records = list(cursor.fetchall())
+        if len(records) != 0:
+            return 'Такой логин уже занят'
+    if len(line) == 0:
+        return 'Одна или несколько строк ничего не содержат'
+    elif len(line) <= 2:
+        return 'Одна или несколько строк слишком короткие, минимальная длина 3'
+    elif line.count(' ') != 0:
+        return 'Одна или несколько строк имеют пробелы'
+    else:
+        for s in line.lower():
+            if s not in alphabet:
+                return 'Одна или несколько строк имеют запрешённые символы'
+        return 'good'
+
+
+if __name__ == '__main__':
+    app.run()
